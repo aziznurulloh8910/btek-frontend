991b0362935626f4cd0de766efc45e12366f6dff
Aziz <aziznurulloh8@gmail.com>
2022-10-28 20:03:07 +0700

    Register Yup Formik

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,37 +1,56 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import YupPassword from 'yup-password';
+import * as Yup from 'yup';
 import http from '../helpers/http';
+
+YupPassword(Yup);
 
 function Register() {
   const navigate = useNavigate();
-  const register = async (e) => {
+
+  const basicAuthSchema = Yup.object().shape({
+    email: Yup.string().email('Email is not valid').required(),
+    password: Yup.string().password().required(),
+  });
+
+  const submitAction = async (values) => {
     try {
-      e.preventDefault();
-      const form = {
-        email: e.target.email.value,
-        password: e.target.password.value,
-      };
-      const encoded = new URLSearchParams(form);
-      const { data } = await http().post('/auth/register', encoded.toString());
-      window.localStorage.setItem('token', data.results.token);
-      navigate('/');
+      const form = new URLSearchParams(values);
+      await http().post('/auth/register', form.toString());
+      navigate('/login');
     } catch (err) {
       window.alert(err.response.data.message);
     }
   };
-
   return (
-    <form onSubmit={register}>
-      Email :
-      <br />
-      <input type="email" name="email" />
-      <br />
-      Password :
-      <br />
-      <input type="password" name="password" />
-      <br />
-      <button type="submit">Register</button>
-    </form>
+    <Formik
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+      validationSchema={basicAuthSchema}
+      onSubmit={submitAction}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field type="text" name="email" />
+          <br />
+          {errors.email && touched.email ? (
+            <div>{errors.email}</div>
+          ) : null}
+          <br />
+          <Field type="password" name="password" />
+          <br />
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null}
+          <br />
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
