9f9d6d515fb5d657dcdbe6f3b6f4c42457193e57
Aziz <aziznurulloh8@gmail.com>
2022-10-29 00:32:35 +0700

    [CHG] Edit profile with Formik and Yup

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,26 +1,41 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import { useNavigate } from 'react-router-dom';
+import YupPassword from 'yup-password';
+import * as Yup from 'yup';
 import http from '../helpers/http';
 
+YupPassword(Yup);
+
 function EditProfile() {
+  const navigate = useNavigate();
+
+  const editProfileSchema = Yup.object().shape({
+    fullName: Yup.string().required(),
+    birthDate: Yup.string().required(),
+    picture: Yup.mixed().required(),
+  });
+
   const [userProfile, setUserProfile] = React.useState({});
   const getProfile = async () => {
     const token = window.localStorage.getItem('token');
     const { data } = await http(token).get('/profile');
     setUserProfile(data.results);
   };
-  const saveData = async (e) => {
-    e.preventDefault();
+
+  const submitAction = async (values) => {
     const token = window.localStorage.getItem('token');
     const form = new FormData();
-    form.append('fullName', e.target.fullName.value);
-    form.append('birthDate', e.target.birthDate.value);
-    form.append('picture', e.target.picture.files[0]);
+    form.append('fullName', values.fullName);
+    form.append('birthDate', values.birthDate);
+    form.append('picture', values.picture);
     const { data } = await http(token).put('/profile', form, {
       headers: {
         'Content-Type': 'multypart/form-data',
       },
     });
     setUserProfile(data.results);
+    navigate('/profile');
   };
 
   React.useEffect(() => {
@@ -30,21 +45,39 @@ function EditProfile() {
   return (
     <>
       {userProfile?.picture && <img style={{ width: '240px', height: '100%' }} src={`http://localhost:8888/assets/uploads/${userProfile?.picture}`} alt={userProfile?.picture} />}
-      <form onSubmit={saveData}>
-        Full Name :
-        <br />
-        <input type="text" name="fullName" defaultValue={userProfile?.fullName} />
-        <br />
-        Picture :
-        <br />
-        <input type="file" name="picture" defaultValue={userProfile?.picture} />
-        <br />
-        Birth Date :
-        <br />
-        <input type="text" name="birthDate" defaultValue={userProfile?.birthDate} />
-        <br />
-        <button type="submit">Save</button>
-      </form>
+      <Formik
+        initialValues={{
+          fullName: '',
+          picture: '',
+          birthDate: '',
+        }}
+        validationSchema={editProfileSchema}
+        onSubmit={submitAction}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field type="text" name="fullName" />
+            <br />
+            {errors.fullName && touched.fullName ? (
+              <div>{errors.fullName}</div>
+            ) : null}
+            <br />
+            <Field type="file" name="picture" />
+            <br />
+            {errors.picture && touched.picture ? (
+              <div>{errors.picture}</div>
+            ) : null}
+            <br />
+            <Field type="text" name="birthDate" />
+            <br />
+            {errors.birthDate && touched.birthDate ? (
+              <div>{errors.birthDate}</div>
+            ) : null}
+            <br />
+            <button type="submit">Save</button>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,7 @@ function Login() {
       >
         {({ errors, touched }) => (
           <Form>
-            <Field type="text" name="email" />
+            <Field type="email" name="email" />
             <br />
             {errors.email && touched.email ? (
               <div>{errors.email}</div>
